17e55f2dc76be9c10408787c227c039a16db1f87
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-05-07 23:15:24 +0600

    finished all of the project

--- backend/executeCpp.js
+++ backend/executeCpp.js
@@ -10,18 +10,18 @@ if (!fs.existsSync(outputPath)) {
 
 const executeCpp = (filepath) => {
   const jobId = path.basename(filepath).split(".")[0];
-  const outPath = path.join(outputPath, `${jobId}.exe`);
+  const outPath = path.join(outputPath, 
+    // `${jobId}.exe`
+    `${jobId}.out`    
+    );
 
   return new Promise((resolve, reject) => {
     exec(
-      `g++ ${filepath} -o ${outPath} && cd ${outputPath} && ${jobId}.exe`,
+      // `g++ ${filepath} -o ${outPath} && cd ${outputPath} && ${jobId}.exe` --[for windows]
+      `g++ ${filepath} -o ${outPath} && cd ${outputPath} && ${jobId}.out`,
       (error, stdout, stderr) => {
-        if (error) {
-          reject({ error, stderr });
-        }
-        if (stderr) {
-          reject(stderr);
-        }
+        error && reject({ error, stderr });
+        stderr && reject(stderr);
         resolve(stdout);
       }
     );
